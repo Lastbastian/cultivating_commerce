39855502de38f559e5b8ece7037e535370ef48c4
Chris Bastian <peacethrubeats@gmail.com>
2014-11-29 15:10:41 -0800

    seeds

--- cultivating_commerce/db/seeds.rb
+++ cultivating_commerce/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(first_name:'Chris', last_name:'Bastian', user_name:'chrisb', email:'peacethrubeats@gmail.com',address_line_1:'277A Duncan st.', zipcode:'94131', about:'', phone:'555-555-5555', private_contact:'true')
+User.create(first_name:'Osman', last_name:'Afridi', user_name:'osmana', email: 'osmanisalso@gmail.com', address_line_1:'', zipcode:'', about:'', phone:'(510) 447-3200', private_contact:'true')
+User.create(first_name:'Marlon', last_name:'Frausto', email:'marlonjfrausto@gmail.com', user_name:'marlonf', address_line_1:'', zipcode:'', about:'', phone:'(619) 373-6451', private_contact:'true')
+User.create(first_name:'Chritine', last_name:'Bastian', email: 'christinevallaure@gmail.com',user_name:'christinev', address_line_1:'', zipcode:'', about:'', phone:'555-555-5555', private_contact:'true')
+
+Listing.create(title:'',description:'',desired:'',quantitiy:'',image:'',active:'',user_id:'')
+
+Item.create(kind:'',category:'')
+
+Event.create(address_line_1:'', zipcode:'', date:'', time:'',title:'', description:'', image:'')
+
