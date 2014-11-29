c6b1e115fe59b50ce8f86dbce9a3440bc45b4d7a
Chris Bastian & Osman <pair+lastbastian.oafridi@devbootcamp.com>
2014-11-29 11:11:46 -0800

    added devise and updated layouts to display login related messages

--- cultivating_commerce/app/models/user.rb
+++ cultivating_commerce/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_one :listing
   has_many :meetups_participants
   has_many :meetups_hosts
