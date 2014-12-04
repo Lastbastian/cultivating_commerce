76b5d7009bb1b2fe59e35ab68c23fe2de951a485
Marlon J. Frausto <marlonjfrausto@Marlons-MacBook-Pro-2.local>
2014-12-03 16:48:40 -0800

    erased white space

--- cultivating_commerce/app/models/listing.rb
+++ cultivating_commerce/app/models/listing.rb
@@ -3,16 +3,12 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   before_save :format
 
-  
   validates :title, 
             :presence => {:message => "Your listing requires a title."}
   validates :description, 
             :presence => {:message => "Your listing requires a description."}
   validates :quantity, 
             :presence => {:message => "Please enter the quantity available for exchange."}
-
-
-
 
   def format
     self.title.downcase!
