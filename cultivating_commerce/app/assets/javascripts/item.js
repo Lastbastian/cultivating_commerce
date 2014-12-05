4ac2a7e2ae9b908da9503c25bd44ca9453a6e6a3
Marlon J. Frausto <marlonjfrausto@Marlons-MacBook-Pro-2.local>
2014-12-04 16:30:08 -0800

    finished filtering

--- cultivating_commerce/app/assets/javascripts/item.js
+++ cultivating_commerce/app/assets/javascripts/item.js
@@ -38,5 +38,74 @@ $(".submit-item").on("click", function(e){
 
  // $('ul').sortable();
 
+// var options = {
+//   valueNames: [ 'listing' ]
+// };
 
+// console.log("here's something")
+
+// var allListings = new List('all-listings', options);
+
+// $('.filter-1').click(function() {
+//   allListings.filter(function(item) {
+//     debugger;
+//     if (item.className == dataset.kind) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+// return false;
+// });
+
+
+$( ".filter-1" ).click(function() {
+  $( "[data-kind=filter-1]" ).show( "slow" );
+  $( "[data-kind=filter-2]" ).hide( "slow" );
+  $( "[data-kind=filter-3]" ).hide( "slow" );
+  $( "[data-kind=filter-4]" ).hide( "slow" );
+  $( "[data-kind=filter-5]" ).hide( "slow" );
+  $( "[data-kind=filter-6]" ).hide( "slow" );
+});
+
+$( ".filter-2" ).click(function() {
+  $( "[data-kind=filter-1]" ).hide( "slow" );
+  $( "[data-kind=filter-2]" ).show( "slow" );
+  $( "[data-kind=filter-3]" ).hide( "slow" );
+  $( "[data-kind=filter-4]" ).hide( "slow" );
+  $( "[data-kind=filter-5]" ).hide( "slow" );
+  $( "[data-kind=filter-6]" ).hide( "slow" );
+});
+$( ".filter-3" ).click(function() {
+  $( "[data-kind=filter-1]" ).hide( "slow" );
+  $( "[data-kind=filter-2]" ).hide( "slow" );
+  $( "[data-kind=filter-3]" ).show( "slow" );
+  $( "[data-kind=filter-4]" ).hide( "slow" );
+  $( "[data-kind=filter-5]" ).hide( "slow" );
+  $( "[data-kind=filter-6]" ).hide( "slow" );
+});
+$( ".filter-4" ).click(function() {
+  $( "[data-kind=filter-1]" ).hide( "slow" );
+  $( "[data-kind=filter-2]" ).hide( "slow" );
+  $( "[data-kind=filter-3]" ).hide( "slow" );
+  $( "[data-kind=filter-4]" ).show( "slow" );
+  $( "[data-kind=filter-5]" ).hide( "slow" );
+  $( "[data-kind=filter-6]" ).hide( "slow" );
+});
+$( ".filter-5" ).click(function() {
+  $( "[data-kind=filter-1]" ).hide( "slow" );
+  $( "[data-kind=filter-2]" ).hide( "slow" );
+  $( "[data-kind=filter-3]" ).hide( "slow" );
+  $( "[data-kind=filter-4]" ).hide( "slow" );
+  $( "[data-kind=filter-5]" ).show( "slow" );
+  $( "[data-kind=filter-6]" ).hide( "slow" );
+});
+$( ".filter-6" ).click(function() {
+  $( "[data-kind=filter-1]" ).hide( "slow" );
+  $( "[data-kind=filter-2]" ).hide( "slow" );
+  $( "[data-kind=filter-3]" ).hide( "slow" );
+  $( "[data-kind=filter-4]" ).hide( "slow" );
+  $( "[data-kind=filter-5]" ).hide( "slow" );
+  $( "[data-kind=filter-6]" ).show( "slow" );
+});
 });
